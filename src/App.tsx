64aee3cdc26e6f8023c78535c27ba515a4f885ca
Anthony Thomas <95666367+anthonythom@users.noreply.github.com>
2023-02-26 03:32:24 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import  MainContent  from './components/MainContent';
+import  Sidebar  from './components/Sidebar';
+import './styles/components/app.sass';
+
+function App() {
+ 
+
+  return (
+    <div id="portfolio">
+    <h1>Anthony Thomas</h1>
+    <Sidebar />
+    <MainContent />
+
+    
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,18 @@
+import Avatar from '../img/eu.jpg' ;
+
+
+import '../styles/components/sidebar.sass';
+
+
+const Sidebar = () => {
+  return <aside id="sidebar">
+ <img src="" alt="" />
+    <p className="title">  Web Developer FullStack</p>
+    <p>redes sociais</p>
+    <p>informações de contato</p>
+    <a href="" className="btn">Download curriculo
+    </a>
+  </aside>
+}
+
+export default Sidebar
